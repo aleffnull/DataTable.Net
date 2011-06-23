d0c4b10d01927cad193771457253b85f97fa9cfb
Mikhail Savkin <savkinmk@gmail.com>
2011-06-23 13:10:22 +0000

    Issue #1. UI of integration tab in Settings form is ready.

--- sources/DataTable.Net/Forms/SettingsForm.Designer.cs
+++ sources/DataTable.Net/Forms/SettingsForm.Designer.cs
@@ -38,9 +38,15 @@
 			this.SettingsTabControl = new System.Windows.Forms.TabControl();
 			this.GeneralTabPage = new System.Windows.Forms.TabPage();
 			this.IntegrationTabPage = new System.Windows.Forms.TabPage();
+			this.FileTypesLabel = new System.Windows.Forms.Label();
+			this.DatFileTypeCheckBox = new System.Windows.Forms.CheckBox();
+			this.HexFileTypeCheckBox = new System.Windows.Forms.CheckBox();
+			this.BinFileTypeCheckBox = new System.Windows.Forms.CheckBox();
+			this.SelectAllFileTypesButton = new System.Windows.Forms.Button();
 			((System.ComponentModel.ISupportInitialize)(this.MaxAbsoluteScalePowerUpDown)).BeginInit();
 			this.SettingsTabControl.SuspendLayout();
 			this.GeneralTabPage.SuspendLayout();
+			this.IntegrationTabPage.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// MaxAbsoluteScalePowerLabel
@@ -63,7 +69,7 @@
 			// 
 			this.OkButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.OkButton.DialogResult = System.Windows.Forms.DialogResult.OK;
-			this.OkButton.Location = new System.Drawing.Point(250, 238);
+			this.OkButton.Location = new System.Drawing.Point(223, 177);
 			this.OkButton.Name = "OkButton";
 			this.OkButton.Size = new System.Drawing.Size(75, 23);
 			this.OkButton.TabIndex = 2;
@@ -74,7 +80,7 @@
 			// 
 			this.CancelationButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.CancelationButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.CancelationButton.Location = new System.Drawing.Point(331, 238);
+			this.CancelationButton.Location = new System.Drawing.Point(304, 177);
 			this.CancelationButton.Name = "CancelationButton";
 			this.CancelationButton.Size = new System.Drawing.Size(75, 23);
 			this.CancelationButton.TabIndex = 3;
@@ -109,7 +115,7 @@
 			this.SettingsTabControl.Location = new System.Drawing.Point(3, 3);
 			this.SettingsTabControl.Name = "SettingsTabControl";
 			this.SettingsTabControl.SelectedIndex = 0;
-			this.SettingsTabControl.Size = new System.Drawing.Size(407, 229);
+			this.SettingsTabControl.Size = new System.Drawing.Size(380, 168);
 			this.SettingsTabControl.TabIndex = 6;
 			// 
 			// GeneralTabPage
@@ -121,40 +127,95 @@
 			this.GeneralTabPage.Location = new System.Drawing.Point(4, 22);
 			this.GeneralTabPage.Name = "GeneralTabPage";
 			this.GeneralTabPage.Padding = new System.Windows.Forms.Padding(3);
-			this.GeneralTabPage.Size = new System.Drawing.Size(399, 203);
+			this.GeneralTabPage.Size = new System.Drawing.Size(399, 142);
 			this.GeneralTabPage.TabIndex = 0;
 			this.GeneralTabPage.Text = "Общие";
 			this.GeneralTabPage.UseVisualStyleBackColor = true;
 			// 
 			// IntegrationTabPage
 			// 
+			this.IntegrationTabPage.Controls.Add(this.SelectAllFileTypesButton);
+			this.IntegrationTabPage.Controls.Add(this.BinFileTypeCheckBox);
+			this.IntegrationTabPage.Controls.Add(this.HexFileTypeCheckBox);
+			this.IntegrationTabPage.Controls.Add(this.DatFileTypeCheckBox);
+			this.IntegrationTabPage.Controls.Add(this.FileTypesLabel);
 			this.IntegrationTabPage.Location = new System.Drawing.Point(4, 22);
 			this.IntegrationTabPage.Name = "IntegrationTabPage";
 			this.IntegrationTabPage.Padding = new System.Windows.Forms.Padding(3);
-			this.IntegrationTabPage.Size = new System.Drawing.Size(575, 336);
+			this.IntegrationTabPage.Size = new System.Drawing.Size(372, 142);
 			this.IntegrationTabPage.TabIndex = 1;
 			this.IntegrationTabPage.Text = "Интеграция";
 			this.IntegrationTabPage.UseVisualStyleBackColor = true;
+			// 
+			// FileTypesLabel
+			// 
+			this.FileTypesLabel.AutoSize = true;
+			this.FileTypesLabel.Location = new System.Drawing.Point(6, 3);
+			this.FileTypesLabel.Name = "FileTypesLabel";
+			this.FileTypesLabel.Size = new System.Drawing.Size(195, 13);
+			this.FileTypesLabel.TabIndex = 0;
+			this.FileTypesLabel.Text = "Открывать файлы следующих типов:";
+			// 
+			// DatFileTypeCheckBox
+			// 
+			this.DatFileTypeCheckBox.AutoSize = true;
+			this.DatFileTypeCheckBox.Location = new System.Drawing.Point(9, 19);
+			this.DatFileTypeCheckBox.Name = "DatFileTypeCheckBox";
+			this.DatFileTypeCheckBox.Size = new System.Drawing.Size(48, 17);
+			this.DatFileTypeCheckBox.TabIndex = 1;
+			this.DatFileTypeCheckBox.Text = "DAT";
+			this.DatFileTypeCheckBox.UseVisualStyleBackColor = true;
+			// 
+			// HexFileTypeCheckBox
+			// 
+			this.HexFileTypeCheckBox.AutoSize = true;
+			this.HexFileTypeCheckBox.Location = new System.Drawing.Point(9, 42);
+			this.HexFileTypeCheckBox.Name = "HexFileTypeCheckBox";
+			this.HexFileTypeCheckBox.Size = new System.Drawing.Size(48, 17);
+			this.HexFileTypeCheckBox.TabIndex = 2;
+			this.HexFileTypeCheckBox.Text = "HEX";
+			this.HexFileTypeCheckBox.UseVisualStyleBackColor = true;
+			// 
+			// BinFileTypeCheckBox
+			// 
+			this.BinFileTypeCheckBox.AutoSize = true;
+			this.BinFileTypeCheckBox.Location = new System.Drawing.Point(9, 65);
+			this.BinFileTypeCheckBox.Name = "BinFileTypeCheckBox";
+			this.BinFileTypeCheckBox.Size = new System.Drawing.Size(44, 17);
+			this.BinFileTypeCheckBox.TabIndex = 3;
+			this.BinFileTypeCheckBox.Text = "BIN";
+			this.BinFileTypeCheckBox.UseVisualStyleBackColor = true;
+			// 
+			// SelectAllFileTypesButton
+			// 
+			this.SelectAllFileTypesButton.Location = new System.Drawing.Point(9, 88);
+			this.SelectAllFileTypesButton.Name = "SelectAllFileTypesButton";
+			this.SelectAllFileTypesButton.Size = new System.Drawing.Size(105, 23);
+			this.SelectAllFileTypesButton.TabIndex = 4;
+			this.SelectAllFileTypesButton.Text = "Выбрать &все";
+			this.SelectAllFileTypesButton.UseVisualStyleBackColor = true;
 			// 
 			// SettingsForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.CancelButton = this.CancelationButton;
-			this.ClientSize = new System.Drawing.Size(411, 273);
+			this.ClientSize = new System.Drawing.Size(384, 212);
 			this.Controls.Add(this.SettingsTabControl);
 			this.Controls.Add(this.CancelationButton);
 			this.Controls.Add(this.OkButton);
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.MaximizeBox = false;
 			this.MinimizeBox = false;
-			this.MinimumSize = new System.Drawing.Size(360, 180);
+			this.MinimumSize = new System.Drawing.Size(400, 250);
 			this.Name = "SettingsForm";
 			this.Text = "Настройки";
 			((System.ComponentModel.ISupportInitialize)(this.MaxAbsoluteScalePowerUpDown)).EndInit();
 			this.SettingsTabControl.ResumeLayout(false);
 			this.GeneralTabPage.ResumeLayout(false);
 			this.GeneralTabPage.PerformLayout();
+			this.IntegrationTabPage.ResumeLayout(false);
+			this.IntegrationTabPage.PerformLayout();
 			this.ResumeLayout(false);
 
 		}
@@ -170,5 +231,10 @@
 		private System.Windows.Forms.TabControl SettingsTabControl;
 		private System.Windows.Forms.TabPage GeneralTabPage;
 		private System.Windows.Forms.TabPage IntegrationTabPage;
+		private System.Windows.Forms.CheckBox BinFileTypeCheckBox;
+		private System.Windows.Forms.CheckBox HexFileTypeCheckBox;
+		private System.Windows.Forms.CheckBox DatFileTypeCheckBox;
+		private System.Windows.Forms.Label FileTypesLabel;
+		private System.Windows.Forms.Button SelectAllFileTypesButton;
 	}
 }
